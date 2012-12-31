90902dba40736524085fd488b0e235de027e7976
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-12-31 16:13:43 +0100

    squashed 2 commands into 1

--- ubuntu/rails.sh
+++ ubuntu/rails.sh
@@ -8,10 +8,8 @@ echo "Install packages. Give your password when asked."
 sudo apt-get install build-essential bison openssl libreadline6 \
   libreadline6-dev curl git git-core zlib1g zlib1g-dev libssl-dev libyaml-dev \
   libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf \
-  libc6-dev ncurses-dev automake libtool nodejs libpq-dev -y
-
-echo "Install ImageMagick for image processing"
-sudo apt-get install imagemagick --fix-missing -y
+  libc6-dev ncurses-dev automake libtool nodejs libpq-dev imagemagick \
+  --fix-missing -y
 
 echo "Install RVM (Ruby Version Manager)"
 curl -L get.rvm.io | bash -s stable
