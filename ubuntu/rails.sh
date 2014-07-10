da4ef9f33f7c3eec5cda33c4f6d24274fa866045
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-07-11 01:21:43 +0200

    apt-get install inside subscript

--- ubuntu/rails.sh
+++ ubuntu/rails.sh
@@ -4,12 +4,15 @@ echo "Update packages. Ask for your password."
 sudo apt-get update -y
 
 echo "Install packages. Give your password when asked."
+bash << +END
 sudo apt-get install build-essential bison openssl libreadline6 \
   libreadline6-dev curl git git-core zlib1g zlib1g-dev libssl-dev libyaml-dev \
   libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf \
   libc6-dev ncurses-dev automake libtool nodejs libpq-dev imagemagick \
   gawk libgdbm-dev libffi-dev libmagickwand-dev libmagickcore-dev \
   libmysqlclient-dev --fix-missing -y
+exit 0
++END
 
 echo "Install RVM (Ruby Version Manager)"
 curl -sSL https://get.rvm.io | bash -s stable
--- ubuntu/tools.sh
+++ ubuntu/tools.sh
@@ -2,9 +2,12 @@
 set -e
 
 ### Tools
+bash << +END
 sudo apt-get install git git-core gitg curl vim zsh nautilus-open-terminal \
   compizconfig-settings-manager compiz-plugins-default compiz-plugins-main \
   synaptic autojump exuberant-ctags tree htop -y
+exit 0
++END
 
 # janus
 curl -Lo- https://bit.ly/janus-bootstrap | bash
